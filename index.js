37eb4fed42fa571d18d24863e0761936b8e8bf1f
azulika <azulika@users.noreply.github.com>
2022-12-23 08:40:21 +0000

    deploy: 85f611968c337a72bba20b90db2141820fcc3ff7

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 
 var data = [
 {
-  url: "https://azulika.github.io/post/another/",
-  title: "First Post",
+  url: "https://azulika.github.io/post/221223/",
+  title: "221223",
   image: null,
   lastmod: "2022-12-23",
   date: "2022-12-23",
-  body: "First Post test 2"
+  body: "221223 Christmas is coming but I have nothing to do. I don\u0026rsquo;t have any plans or events to attend and I\u0026rsquo;m feeling a bit down about it. I know the holiday is about spending time with loved ones, but with the pandemic still raging on, it\u0026rsquo;s hard to feel the holiday cheer this year. I\u0026rsquo;m trying to stay positive and focus on the things I\u0026rsquo;m grateful for, but it\u0026rsquo;s tough. On a brighter note, I switched my hosting service from Vercel to github.io because Vercel was just too slow for my liking. I\u0026rsquo;ve been really happy with the switch so far and the site is loading much faster. It\u0026rsquo;s the little things, right? In other news, I\u0026rsquo;ve been struggling with chronic sleep deprivation lately. It\u0026rsquo;s been a constant battle and I\u0026rsquo;m having a hard time getting enough rest. I\u0026rsquo;ve tried everything from setting a consistent bedtime to drinking chamomile tea before bed, but nothing seems to be working. It\u0026rsquo;s frustrating and I\u0026rsquo;m feeling pretty drained as a result. On a positive note, I\u0026rsquo;ve been doing a mini digital detox lately where I use the internet but don\u0026rsquo;t use social media. It\u0026rsquo;s been going really well so far and I\u0026rsquo;ve been able to focus more on other things. It\u0026rsquo;s been a nice break from the constant notifications and I\u0026rsquo;ve been able to get a lot more done as a result. Speaking of the internet, I\u0026rsquo;ve noticed a weird trend recently of people using the emoji 💀 to mean \u0026ldquo;I\u0026rsquo;m dead\u0026rdquo;, meaning laughed too hard. I find it a bit morbid, but that\u0026rsquo;s what I like. Relatively decent movement on the internet in a while. Overall, it\u0026rsquo;s been a bit of a mixed bag lately. Some good, some not so good. But I\u0026rsquo;m trying to stay positive and focus on the things that are going well."
 },
 {
   url: "https://azulika.github.io/post/",
@@ -16,5 +16,13 @@ var data = [
   lastmod: "2022-12-23",
   date: "2022-12-23",
   body: "Posts"
+},
+{
+  url: "https://azulika.github.io/post/another/",
+  title: "First Post",
+  image: null,
+  lastmod: "2022-12-23",
+  date: "2022-12-23",
+  body: "First Post test 2"
 },
 ]
