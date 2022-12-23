bfebc594b48d25bb386b6433316abc6f92226794
azulika <azulika@users.noreply.github.com>
2022-12-23 06:33:06 +0000

    deploy: 0d1eaccf1840e64402d8120e64656f2f12dd3c14

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+var data = [
+{
+  url: "https://azulika.github.io/post/another/",
+  title: "First Post",
+  image: null,
+  lastmod: "2022-12-23",
+  date: "2022-12-23",
+  body: "First Post test 2"
+},
+{
+  url: "https://azulika.github.io/post/",
+  title: "Posts",
+  image: null,
+  lastmod: "2022-12-23",
+  date: "2022-12-23",
+  body: "Posts"
+},
+]
