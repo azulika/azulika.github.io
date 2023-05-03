d31e63ef49b2b113e678cc01b4fa2faf4e0182f7
azul372 <mvobujd237@gmail.com>
2023-05-03 14:53:30 +0900

    posts to post/

--- tina/config.ts
+++ tina/config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from "tinacms";
-import { postFields } from "./templates";
 
 // Your hosting provider likely exposes this as an environment variable
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: "3e3f8256-8de9-497f-8277-6cb2c33a9018", // Get this from tina.io
+  clientId: "3e3f8256-8de9-497f-8277-6cb2c33a9018", // Get this from tina.io
   token: "c41699baa841bc2d6f242c68d70f1e675a7430d4", // Get this from tina.io
-  client: { skip: true },
+
   build: {
     outputFolder: "admin",
     publicFolder: "static",
@@ -22,44 +21,23 @@ export default defineConfig({
   schema: {
     collections: [
       {
-        format: "md",
-        label: "Pages",
-        name: "pages",
-        path: "content",
-        frontmatterFormat: "toml",
-        frontmatterDelimiters: "+++",
-        match: {
-          include: "*",
-        },
-        fields: [
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body of Document",
-            description: "This is the markdown body",
-            isBody: true,
-          },
-        ],
-      },
-      {
-        format: "md",
-        label: "Post",
         name: "post",
-        path: "content/post",
-        frontmatterFormat: "toml",
-        frontmatterDelimiters: "+++",
-        match: {
-          include: "**/*",
-        },
+        label: "Posts",
+        path: "content/posts",
         fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
           {
             type: "rich-text",
             name: "body",
-            label: "Body of Document",
-            description: "This is the markdown body",
+            label: "Body",
             isBody: true,
           },
-          ...postFields(),
         ],
       },
     ],
